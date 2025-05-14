03076f06a9c8fe0527936027ca1b947bb3c3eb2b
Sameem <211040@oxfordcollege.edu.np>
2025-05-14 15:48:03 +0545

    Back-End Laravel

---
+++ Ai-Job-Portal-Laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $Validate=Validator::make($request->all(),[
+            'fullname'     => 'required|string|max:100',
+            'email'        => 'required|email|unique:users,email',
+            'password'     =>  'required|string|min:8'
+        ]);
+
+        //validation
+        if($Validate->fails()){
+            $Errors=$Validate->errors();
+
+            $ErrorMessage="";
+            if($ErrorMessage->has('email')) $ErrorMessage='The email has already been taken.';
+            else $ErrorMessage ='validation error';
+
+            return response()->json([
+                'error'  =>true,
+                'reason'  => $ErrorMessage,
+                'response'  =>null
+            ]);
+        }
+        //register new user
+        $User=User::create($request->all());
+
+        //return the new user
+        return response()->json([
+            'error'   =>false,
+            'reason' =>'listed',
+            'response'   =>$User
+        ]);
+    }
+
+    //Login
+    public function login(Request $request){
+        $Validate=Validator::make($request->all(),[
+            'email'   => 'required|email',
+            'password'=> 'required|string|min:8'
+        ]);
+
+        //validate
+        if ($Validate->fails()){
+            return response()->json([
+                'error'   =>true,
+                'reason'  =>'validation error',
+                'response' =>null
+            ]);
+        }
+
+
+            $User=User::where('email',$request->email)->first();
+            if(!$User || !Hash::check($request->password,$User->password)){
+                return response()->json([
+                    'error'   =>true,
+                    'reason'  =>"Email and password didnt match",
+                    'response'  =>null
+                ]);
+            }
+        //create token and return
+
+
+        $token =$User->createToken('user')->plainTextToken;
+
+        return response()->json([
+            'error'   =>false,
+            'reason'   =>'success',
+            'response'  =>array(
+                'user'    =>$User,
+                'token'   =>$token
+            )
+        ]);
+    }
+
+    //logout
+
+    public function logout (Request $request,$id){
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'error'  =>false,
+            'reason'   =>'success',
+            'response'   =>null
+        ]);
+    }
+}
---
+++ Ai-Job-Portal-Laravel/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+//user register
+Route::post('user/register',[AuthController::class,'register']);
+
+//user login
+Route::post('user/login',[AuthController::class,'login']);
+
+//user logout
+
+Route::group(['middleware'=>['auth:sanctum']],function(){
+
+    //user logout
+    Route::get('user/logout/{id}',[AuthController::class,'logout']);
+
+});
